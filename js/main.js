2f9c898436636b15f4254ca9593a8af7d51297af
Marty Boggs <m@thriveline.com>
2017-04-13 01:09:01 -0700

    ga

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 {% if site.github != nil %}
-//	ga('create', 'UA-52138306-3', 'auto');
-//	ga('send', 'pageview');
+	ga('create', 'UA-52138306-4', 'auto');
+	ga('send', 'pageview');
 {% endif %}
 
 WebFont.load({
